c5ccbd5ea27d72342efb19e8ae5ec5ddc6c57774
tudouMan <liu18255603500@sina.com>
2020-09-15 16:24:49 +0800

    测试了下帧同步

--- TDFrameWork/Assets/TempTest.cs
+++ TDFrameWork/Assets/TempTest.cs
@@ -14,37 +14,145 @@ using TDFramework.Extention;
 public class TempTest : MonoBehaviour
 {
 
-    StateFactory sf = new StateFactory();
+    public readonly int mFixedFrameTime = 10;
+    public int mCurFrameIndexTime;
+    public int mCurFrameIndex;
+    private Dictionary<int, TestFrameProto> mAllFrameOperations = new Dictionary<int, TestFrameProto>();
 
-    IState curState;
-    private void OnGUI()
+    TestFrameProto mLastFrame=new TestFrameProto {FrameIndex=0,mDirect=-1,ID=1 };
+    private PlayeyCtr playerCtr;
+
+    bool isStart = false;
+
+
+    ushort protoType = 30000;
+    private void Awake()
     {
-      
-        if (GUI.Button(new Rect(100, 200, 100, 100), "set"))
+        playerCtr = GetComponent<PlayeyCtr>();
+        EventCenter.AddListener<Byte[]>(protoType, AcceptMsg);
+    }
+
+    private void AcceptMsg(byte[] args)
+    {
+        TestFrameProto acceptProto=TestFrameProto.GetProto(args);
+        if (acceptProto.FrameIndex == -1)
         {
-           
-            sf.RegisterClassAttribute<GameStateAttribute>(AssemblyUtil.DefaultCSharpAssembly);
-            curState= sf.ToState("IdleState");
+            //无效帧
+            //直接恢复上一帧
+            playerCtr.SetDirect(mLastFrame.mDirect);
+            Debug.Log($"玩家{acceptProto.ID} 收到无效帧 执行上次帧{mLastFrame.FrameIndex} 方向为{mLastFrame.mDirect}");
+
         }
-        if (GUI.Button(new Rect(100, 300, 100, 100), "set"))
+        else
         {
-
-
-            curState= sf.ToState("RunState");
+            playerCtr.SetDirect(acceptProto.mDirect);
+            mLastFrame = acceptProto;
+            Debug.Log($"玩家{acceptProto.ID} 收到{acceptProto.FrameIndex}帧 执行 方向为{acceptProto.mDirect}");
         }
     }
 
 
-    private void Update()
+
+
+    private void OnDestroy()
     {
-        if (curState != null)
-            curState.OnUpdate(Time.deltaTime);
+        EventCenter.RemoveListener<Byte[]>(protoType, AcceptMsg);
     }
 
- 
+    public void OnGUI()
+    {
+        if(GUI.Button(new Rect(100, 100, 100, 100), "connect"))
+        {
+            NetWorkSocket.Instance.Connect("192.168.2.188", 1015);
+        }
 
-  
-   
+        if (GUI.Button(new Rect(100, 200, 100, 100), "start"))
+        {
+            isStart = true;
+        }
+    }
+
+
+    // 0 帧开始 
+    private void FixedUpdate()
+    {
+        if (isStart)
+        {
+            if (mCurFrameIndexTime >= mFixedFrameTime)
+            {
+
+                //Send CurFrame Operation
+                if (mAllFrameOperations.ContainsKey(mCurFrameIndex))
+                {
+                    NetWorkSocket.Instance.SendMsg(mAllFrameOperations[mCurFrameIndex].ToArray());
+                }
+                else
+                {
+                    //Send CurFrame Operation null frame
+                    NetWorkSocket.Instance.SendMsg(mLastFrame.ToArray());
+                }
+               
+              
+                mCurFrameIndexTime = 0;
+                mCurFrameIndex++;
+            }
+            else
+            {
+                mCurFrameIndexTime++;
+            }
+        }
+       
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            TestFrameProto operation = new TestFrameProto { FrameIndex= mCurFrameIndex ,mDirect=0,ID=1};
+            if (!mAllFrameOperations.ContainsKey(mCurFrameIndex))
+            {
+                mAllFrameOperations.Add(mCurFrameIndex, operation);
+            }
+            mAllFrameOperations[mCurFrameIndex] = operation;
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TestFrameProto operation = new TestFrameProto { FrameIndex = mCurFrameIndex, mDirect = 1,ID=1 };
+            if (!mAllFrameOperations.ContainsKey(mCurFrameIndex))
+            {
+                mAllFrameOperations.Add(mCurFrameIndex, operation);
+            }
+            mAllFrameOperations[mCurFrameIndex] = operation;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+
+            TestFrameProto operation = new TestFrameProto { FrameIndex = mCurFrameIndex, mDirect = -1,ID=1 };
+            if (!mAllFrameOperations.ContainsKey(mCurFrameIndex))
+            {
+                mAllFrameOperations.Add(mCurFrameIndex, operation);
+            }
+            mAllFrameOperations[mCurFrameIndex] = operation;
+
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            TestFrameProto operation = new TestFrameProto { FrameIndex = mCurFrameIndex, mDirect = -1,ID=1 };
+            if (!mAllFrameOperations.ContainsKey(mCurFrameIndex))
+            {
+                mAllFrameOperations.Add(mCurFrameIndex, operation);
+            }
+            mAllFrameOperations[mCurFrameIndex] = operation;
+        }
+    }
 }
 
 
---
+++ TDFrameWork/Assets/TestFrameProto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public struct TestFrameProto
+{
+    public ushort ProtoCode => 30000;
+
+    public int ID;
+
+    public int FrameIndex;
+
+    public int mDirect;   //0 代表Left 1 Right
+
+    public byte[] ToArray()
+    {
+        using (ByteMemoryStream ms = new ByteMemoryStream())
+        {
+            ms.WriteUShort(ProtoCode);
+            ms.WriteInt(ID);
+            ms.WriteInt(FrameIndex);
+            ms.WriteInt(mDirect);
+            return ms.ToArray();
+        }
+    }
+
+    public static TestFrameProto GetProto(byte[] buffer)
+    {
+        TestFrameProto proto = new TestFrameProto();
+        using (ByteMemoryStream ms = new ByteMemoryStream(buffer))
+        {
+            proto.ID = ms.ReadInt();
+            proto.FrameIndex = ms.ReadInt();
+            proto.mDirect = ms.ReadInt();
+        }
+        return proto;
+    }
+}
+
