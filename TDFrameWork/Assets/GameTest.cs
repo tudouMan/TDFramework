fbbf72123630d4ffe4333e9614cf817b6a25c2bb
tudouMan <liuqiang@dobest.com>
2021-06-09 15:37:01 +0800

    测试UI

---
+++ TDFrameWork/Assets/GameTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TDFramework;
+
+public class GameTest : MonoBehaviour
+{
+    private GameObject m_GameObject;
+    private void Awake()
+    {
+        GameEntry.Instance.OnSingletonInit();
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(100, 100, 100, 100), "pop"))
+        {
+            GameEntry.Pool.PopPrefab("TestPrefab", p => m_GameObject = p.gameObject);
+        }
+        if (GUI.Button(new Rect(100, 200, 100, 100), "push"))
+        {
+            GameEntry.Pool.PushPrefab(m_GameObject);
+        }
+
+    }
+}
