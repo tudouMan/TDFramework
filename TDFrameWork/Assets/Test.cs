98a508e46beb306509bb5ee9d3222a7385c7b375
tudouMan <liuqiang@dobest.com>
2021-10-08 16:25:00 +0800

    接入odin插件

--- TDFrameWork/Assets/Test.cs
+++ TDFrameWork/Assets/Test.cs
@@ -5,24 +5,57 @@ using System.IO;
 using TDFramework;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-
-
+using Sirenix.Serialization;
+using Sirenix.OdinInspector;
 
 public class Test : MonoBehaviour
 {
+
+    public Dictionary<int, UnityEngine.GameObject> Dic = new Dictionary<int, GameObject>();
+
+ 
+
     private void Awake()
     {
         GameEntry.Instance.Init();
     }
 
+    [Button("Set")]
+    private void Set()
+    {
+
+    }
 
     private void OnGUI()
     {
         if(GUI.Button(new Rect(100, 100, 100, 100), "set"))
         {
-            Debug.Log("set:" + GameEntry.Table.m_TestTable.Get(1).set.ToString());
+            int outside = 0;
+            MethdInvoke[] methds = new MethdInvoke[2];
+            for (int index = 0; index < 2; index++)
+            {
+                int inside=0;
+                methds[index] = delegate 
+                {
+                    Debug.Log($"outside:{outside.ToString()} inside:{inside.ToString()}");
+                    inside++;
+                    outside++;
+                };
+            }
+
+
+            methds[0]();
+            methds[0]();
+            methds[0]();
+            methds[1]();
+            methds[1]();
         }
     }
+
+
+    public delegate void MethdInvoke();
+
+
 }
 
 
